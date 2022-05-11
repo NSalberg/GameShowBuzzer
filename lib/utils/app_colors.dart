55b6960cd66b4c187b940fd4eb9ba93781140645
Nathan Salberg <Salbe018@d.umn.edu>
2022-05-10 20:49:14 -0500

    standard app colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+final Color background = Color(0xFFfafafc);
+final Color sliverBackground = Color(0xFFfafafc);
+final Color menu1Color = Color(0xFFf9d263);
+final Color menu2Color = Color(0xFFf2603d);
+final Color menu3Color = Color(0xFF04abe6);
+final Color tabVarViewColor = Color(0xFFfdfdfd);
+final Color starColor = Color(0xFFfdc746);
+final Color subTitleText = Color(0xFFc5c4ca);
+final Color loveColor = Color(0xFF00ace6);
+final Color audioBluishBackground = Color(0xFfdee7fa);
+final Color audioBlueBackground = Color(0xFF04abe7);
+final Color audioGreyBackground = Color(0xFFf2f2f2);
+
+//Color
+
+final Color darkBackground = Color(0xFF282a37);
+final Color darkBackground2 = Color(0xFF37304c);
+final Color darkBackgroundSecondary = Color(0xFF414149);
+
+final Color textField = Color(0xFF323645);
+final Color textFieldSelect = Color(0xFF37304c);
+final Color textFieldText = Color(0xFF656774);
+final Color lightAccent = Color(0xFFd27942);
+final Color darkAccent = Color(0xff682d01);
+
+
+/* Another Color Palette Option
+final Color darkBackground = Color(0xFF443C39);
+final Color darkBackground2 = Color(0xff4c3b30);
+final Color darkBackgroundSecondary = Color(0xFF9A7D72);
+
+final Color textField = Color(0xFF5F4E45);
+final Color textFieldSelect = Color(0xff4c3930);
+final Color textFieldText = Color(0xFF998A85);
+final Color lightAccent = Color(0xfff89766); 
+final Color darkAccent = Color(0xffce7e53); */
